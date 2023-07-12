b0306dacf4dc6598288106cc92d82ffba64c1e5a
kouta01 <138412533+kouta01@users.noreply.github.com>
2023-07-12 11:30:34 +0900

    Match3

--- GameProgramming/Match3/AppMain.cpp
+++ GameProgramming/Match3/AppMain.cpp
@@ -1,21 +1,93 @@
-#include "DxLib.h"
+/************************************
+* プログラミング実習
+* マッチ3ゲームの制作
+************************************/
 
-int WINAPI WinMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE hPrevInstance, _In_
-	LPSTR lpCmdine, _In_ int nShowCmd)
+#include"DxLib.h"
+
+#include"FreamControl.h"
+
+#include"InputControl.h"
+
+#include"SceneManager.h"
+
+/************************************
+*マクロ定義
+************************************/
+
+#define SCREEN_HEIGHT   (480)  //スクリーンサイズ（高さ）
+#define SCREEN_WIDTH    (640)  //スクリーンサイズ（幅）
+#define SCREEN_COLORBIT (32)   //スクリーンカラービット
+#define FONT_SIZE       (20)   //文字サイズ
+
+/************************************
+*グローバル変数宣言
+************************************/
+
+
+/************************************
+*プロトタイプ宣言
+************************************/
+
+/************************************
+*プログラムの開始
+************************************/
+
+int WINAPI WinMain(_In_HINSTANCE hInstance, _In_opt_HINSTANCE hPrevInstance,
+_In_LPSTR lpCmdLine, _In_int nshowCmd)
 {
-	//ウィンドウモードで起動
+	//ウィンドウタイトル設定
+	SetMainWindowText("Match 3 Puzzle");
+	//ウィンドモードで起動
 	ChangeWindowMode(TRUE);
+	//画面サイズの最大サイズ,カラービット数を設定
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
 
-	//Dxライブラリの初期化処理
-	if (DxLib_Init() == -1)
-
+	//Dxライブラリ初期化処理
+	//エラーが発生したら、終了する
+	if (DxLib_Init() == D_ERROR)
 	{
-		return -1;
+		return D_ERROR;
 	}
 
-	//入力待機
-	WaitKey();
+	//各機能の初期化処理
+	FreamControl_Initialize();　　　　　//フレームレート制御機能
+	Input_Initialize();                 //入力制御機能
+	//シーンマネージャー初期化処理
+	//エラーが発生したら、終了する
+	if (SceneManager_Initialize(E_TITLE) == D_ERROR)
+	{
+		return D_ERROR;
+	}
 
+	//描画先画面を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定
+	SetFontsize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessage() != D_ERROR && Input_Escape() == FALSE)
+	{
+		//入力制御機能更新処理
+		Input_Update();
+
+		//シーンマネージャー更新処理
+		SceneManager_Update();
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//シーンマネージャー描画処理
+		SceneManager_Draw();
+
+		//フレームレート制御処理
+		FreamControl_update();
+
+		//画面の内容を表画面に反映
+		ScreenFlip();
+
+	}
 	//Dxライブラリ使用の終了処理
 	DxLib_End();
 
